adf51e171dd66431d77fb7e6394c654d5a228ab2
Luis Neves <luis.neves@gmail.com>
2026-03-27 15:21:01 +0000

    fix MariaDB init

--- src/main/java/org/oltp1/initdb/DbInitializer.java
+++ src/main/java/org/oltp1/initdb/DbInitializer.java
@@ -50,7 +50,6 @@ public class DbInitializer
 
 	private void initMariaDb() throws Exception
 	{
-		// Step 0: Create Database
 		// Clean slate, drop everything first
 		executeCommand(sql2oInit, "DROP DATABASE IF EXISTS tpce;");
 		executeCommand(sql2oInit, "CREATE DATABASE tpce;");
@@ -67,38 +66,32 @@ public class DbInitializer
 
 		final SqlScriptExecutor scriptExecutor = new SqlScriptExecutor(sqlContext);
 
-		// Step 1: Create tables
 		log.info("Creating tables...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("1_create_table.sql"));
 
-		// Step 2: Load data using bulk loading
 		log.info("Loading data...");
 
-		executeCommand(sqlContext.getSql2o(), "SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;");
-		executeCommand(sqlContext.getSql2o(), "SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;");
+		executeCommand(sqlContext.getSql2o(), "SET UNIQUE_CHECKS=0;");
+		executeCommand(sqlContext.getSql2o(), "SET FOREIGN_KEY_CHECKS=0;");
 
 		BulkLoader bulkLoader = BulkLoaderFactory.createBulkLoader(sqlContext, dataDir);
 		bulkLoader.loadAllTables();
 
-		executeCommand(sqlContext.getSql2o(), "SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;");
-		executeCommand(sqlContext.getSql2o(), "SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;");
+		executeCommand(sqlContext.getSql2o(), "SET UNIQUE_CHECKS=1;");
+		executeCommand(sqlContext.getSql2o(), "SET FOREIGN_KEY_CHECKS=1;");
 
-		// Step 3: Create indexes and Foreign keys
 		log.info("Creating indexes and Foreign keys...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("3_create_index_fk.sql"));
 
-		// Step 4: Create sequences
 		log.info("Creating sequences...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("4_create_sequence.sql"));
 
-		// Step 5: Database settings
 		log.info("Applying database settings...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("5_db_settings.sql"));
 	}
 
 	private void initPostgresql() throws Exception
 	{
-		// Step 0: Create Database
 		log.info("Creating database...");
 		String oltp1Env = fetchValue(sql2oInit, String.class, "SHOW oltp1.environment;");
 
@@ -139,41 +132,32 @@ public class DbInitializer
 
 		final SqlScriptExecutor scriptExecutor = new SqlScriptExecutor(sqlContext);
 
-		// Step 1: Create tables
 		log.info("Creating tables...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("1_create_table.sql"));
 
-		// Step 2: Load data using bulk loading
 		log.info("Loading data...");
 		BulkLoader bulkLoader = BulkLoaderFactory.createBulkLoader(sqlContext, dataDir);
 		bulkLoader.loadAllTables();
 
-		// Step 3: Create primary keys
 		scriptExecutor.executeScriptFromResource(getScriptPath("3_create_keys.sql"));
 
-		// Step 4: Create indexes
 		log.info("Creating indexes...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("4_create_index.sql"));
 
-		// Step 5: Create foreign keys
 		log.info("Creating foreign keys...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("5_create_fk.sql"));
 
-		// Step 6: Create sequences
 		log.info("Creating sequences...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("6_create_sequence.sql"));
 
-		// Step 7: Analyze tables
 		scriptExecutor.executeScriptFromResource(getScriptPath("7_analyze_table.sql"));
 
-		// Step 8: Database settings
 		log.info("Applying database settings...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("8_db_settings.sql"));
 	}
 
 	private void initMssql() throws Exception
 	{
-		// Step 0: Create Database
 		// Clean slate, drop everything first
 		executeCommand(sql2oInit, "DROP DATABASE IF EXISTS tpce;");
 		executeCommand(sql2oInit, "CREATE DATABASE tpce;");
@@ -192,30 +176,23 @@ public class DbInitializer
 		log.info("Creating tables...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("1_create_table.sql"));
 
-		// Step 2: Load data using bulk loading
 		log.info("Loading data...");
 		BulkLoader bulkLoader = BulkLoaderFactory.createBulkLoader(sqlContext, dataDir);
 		bulkLoader.loadAllTables();
 
-		// Step 3: Create primary keys
 		scriptExecutor.executeScriptFromResource(getScriptPath("3_create_pk.sql"));
 
-		// Step 4: Create indexes
 		log.info("Creating indexes...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("4_create_index.sql"));
 
-		// Step 5: Create foreign keys
 		log.info("Creating foreign keys...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("5_create_fk.sql"));
 
-		// Step 6: Create sequences
 		log.info("Creating sequences...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("6_create_sequence.sql"));
 
-		// Step 7: Analyze tables
 		scriptExecutor.executeScriptFromResource(getScriptPath("7_analyze_table.sql"));
 
-		// Step 8: Database settings
 		log.info("Applying database settings...");
 		scriptExecutor.executeScriptFromResource(getScriptPath("8_db_settings.sql"));
 	}
